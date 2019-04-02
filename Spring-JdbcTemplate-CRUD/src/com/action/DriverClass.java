3f93e34e85665f94d8eb781e2ac5ffca8c257c95
Akash <Akash@192.168.1.105>
2019-04-02 22:36:14 +0500

    Under Construction

--- Spring-JdbcTemplate-CRUD/src/com/action/DriverClass.java
+++ Spring-JdbcTemplate-CRUD/src/com/action/DriverClass.java
@@ -18,4 +18,26 @@ public class DriverClass {
 			System.out.println(employee.getEmployeeId()+" "+employee.getName()+" "+employee.getAge());
 		}
 	}
+	
+	// Below is under work
+	
+/*	System.out.println("Inserting employee!!");
+	Employee newEmp1 = new Employee(1, "John");
+	Employee newEmp2 = new Employee(2, "ASB"); dao.inserEmployeeDetails(newEmp1); 
+	dao.inserEmployeeDetails(newEmp2); 
+	 printEmployees(dao.getAllEmployees()); 
+
+	System.out.println("Updating employee!!");
+	newEmp1.setName("Mark");
+	dao.updateEmployee(newEmp1);
+	printEmployees(dao.getAllEmployees()); 
+
+	System.out.println("Deleting employee Code 1");
+	dao.deleteEployee(1);
+	printEmployees(dao.getAllEmployees()); 
+	}
+
+	public static void printEmployees(List<Employee> empList){    
+	for(Employee emp : empList) {        
+	System.out.println(emp.toString());*/
 }
