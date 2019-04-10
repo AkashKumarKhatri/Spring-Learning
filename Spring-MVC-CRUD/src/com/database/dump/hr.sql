b1f35825acf7fbb212138cc15a3d1b74a1834234
Akash <Akash@192.168.1.105>
2019-04-10 22:31:09 +0500

    database dump

---
+++ Spring-MVC-CRUD/src/com/database/dump/hr.sql
@@ -0,0 +1,38 @@
+/*
+SQLyog Ultimate v11.33 (64 bit)
+MySQL - 5.7.11-log : Database - hr
+*********************************************************************
+*/
+
+/*!40101 SET NAMES utf8 */;
+
+/*!40101 SET SQL_MODE=''*/;
+
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+CREATE DATABASE /*!32312 IF NOT EXISTS*/`hr` /*!40100 DEFAULT CHARACTER SET utf8 */;
+
+USE `hr`;
+
+/*Table structure for table `employees` */
+
+DROP TABLE IF EXISTS `employees`;
+
+CREATE TABLE `employees` (
+  `employee_id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(245) DEFAULT NULL,
+  `salary` int(45) DEFAULT NULL,
+  `desination` varchar(245) DEFAULT NULL,
+  PRIMARY KEY (`employee_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8;
+
+/*Data for the table `employees` */
+
+insert  into `employees`(`employee_id`,`name`,`salary`,`desination`) values (2,'Akash test',1000,'Trainy'),(3,'Roshan Kumar1',20000,'Trainy1'),(4,'1',1,'1');
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
